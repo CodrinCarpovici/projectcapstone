466587b2d9ef2c44e492df4372f8b248f3afceea
Codrin Gabriel Carpovici <gabriel_9729@yahoo.co.uk>
2023-03-08 14:08:08 +0000

    semantic html set up done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     Homepage
-    </div>
+    <>
+      <nav></nav>
+      <header>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p></p>
+        <button></button>
+        <img></img>
+      </header>
+      <main>
+        <section>
+          Specials
+          <button></button>
+          <article></article>
+          <article></article>
+          <article></article>
+        </section>
+        <section>
+          Testimonials
+          <article></article>
+          <article></article>
+          <article></article>
+          <article></article>
+        </section>
+        <section>
+          About
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p></p>
+          <img></img>
+          <img></img>
+        </section>
+      </main>
+      <footer>
+        <img></img>
+      </footer>
+    </>
   );
 }
 
